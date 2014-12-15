d429b1fe3b8136b7854977814ec9b3c639212716
lfsun <lfsun@Ctrip.com>
2014-12-15 22:42:47 +0800

    c# study project

---
+++ CSharpDEV.Collection/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpDEV.Collection
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine(CTest.Add(100,200));
+            Console.WriteLine("Hello World");
+        }
+    }
+}
