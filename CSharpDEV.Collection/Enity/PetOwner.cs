9e1619957e08bf4badf87cb75c03126ddca2e560
lfsun <lfsun@Ctrip.com>
2014-12-17 15:36:53 +0800

    宠物商店修改

--- CSharpDEV.Collection/Enity/PetOwner.cs
+++ CSharpDEV.Collection/Enity/PetOwner.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace CSharpDEV.Collection.Enity
 {
+    /// <summary>
+    /// 宠物商店
+    /// </summary>
     public class PetOwner
     {
         public string Name { get; set; }
