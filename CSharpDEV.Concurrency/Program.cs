d04035db6f1ee8574ea0c3eead77ea0c5e136cc1
sunlf <longfeiit@126.com>
2015-07-25 15:54:27 +0800

    concurrency

---
+++ CSharpDEV.Concurrency/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpDEV.Concurrency
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TaskTest();
+        }
+
+        static void TaskTest()
+        {
+            Task<int> primeNumber = Task.Run(() =>
+                Enumerable.Range(2, 300000).Count(n => Enumerable.Range(2, (int)Math.Sqrt(n) - 1).All(i => n / i > 0)));
+            var awatiter = primeNumber.GetAwaiter();
+            awatiter.OnCompleted(() =>
+            {
+                int result = awatiter.GetResult();
+                Console.WriteLine("The result is" + result);
+            });
+            Console.ReadKey();
+        }
+    }
+}
